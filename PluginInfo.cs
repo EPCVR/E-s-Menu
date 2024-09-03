326bc2175b636537c1def094a15b59e5dc27f5f7
EPCVR <daniboychamp3@gmail.com>
2024-09-02 19:52:29 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.EPCVR.gorillatag.menutemplate";
+        public const string Name = "E's Menu";
+        public const string Description = "Created by Elemental";
+        public const string Version = "1.2.1";
+    }
+}
